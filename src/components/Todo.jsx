8caf9278c2049279234139bc6d194ce7d48b9221
Miquel Carnot <mcgraphicss@gmail.com>
2024-06-14 13:36:04 +0200

    prueba con verified

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
+export const Todo = ({ task, toggleComplete, deleteTodo }) => {
     return (
         <div className="Todo">
-            <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ""}`}>{task.task}</p>
-            
+            <input 
+                type="checkbox" 
+                checked={task.completed} 
+                onChange={() => toggleComplete(task.id)} 
+            />
+            <p className={`${task.completed ? 'completed' : ''}`}>
+                {task.task}
+            </p>
             <div>
-            <FontAwesomeIcon icon={faTrash} onClick= {() => deleteTodo(task.id)} />
+                <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Todo;
